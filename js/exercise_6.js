75c8554deafee22baeb9aa812663b22adf4bc0e8
john Doe <negarshahbazi.official@gmail.com>
2023-11-23 16:26:42 +0100

    third commit

---
+++ js/exercise_6.js
@@ -0,0 +1,5 @@
+
+function nombre(nombre1,nombre2){
+return nombre1%nombre2;
+}
+console.log(nombre(12,2));
---
+++ js/exercise_7.js
@@ -0,0 +1,4 @@
+function pointure(pointure,annéeDeNaissance){
+return ((((pointure*2)+5)*50)-annéeDeNaissance)+1766;
+}
+alert(pointure(38,1991));
