39a054d030f13f6ddd755fcf87bae486d265d1e7
john Doe <negarshahbazi.official@gmail.com>
2023-11-24 09:16:56 +0100

    exercise 10

---
+++ js/exercise_10.js
@@ -0,0 +1,4 @@
+let mot=prompt("saisir un mot");
+for(i=0;i<=mot.length;i++){
+    console.log(mot[i]);
+}
---
+++ js/exercise_9.js
@@ -0,0 +1,4 @@
+let nombre =prompt("saisir un nombre");
+for(i=1;i<=10;i++){
+    console.log(nombre*i);
+}
